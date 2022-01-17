fe5be310ba20e825f77b77d30fca45073e5b52e2
Collin Rukundo <collin@matchstick.ug>
2022-01-17 12:24:53 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'GLYN CMS Manual'
+copyright = '2021, Matchstick 256 Studio Limited'
+author = 'Matchstick 256 Studio'
 
 release = '0.1'
 version = '0.1.0'
